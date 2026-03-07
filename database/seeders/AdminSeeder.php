fc1c5e590a1f8669dee09aab4ef771dd8f91afd0
bernardjbs <bernardjbs@yahoo.com>
2026-03-07 16:31:01 +0800

    feat: add AdminSeeder for single admin user

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name'     => 'Bernard',
+            'email'    => env('ADMIN_EMAIL'),
+            'password' => Hash::make(env('ADMIN_PASSWORD')),
+        ]);
+    }
+}
